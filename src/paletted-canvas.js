4da399ed1dd4b01a882f846fcb6bca923232a3c3
leikareipa <drilledicecream@gmail.com>
2022-06-12 02:43:07 +0300

    Include alpha channel in palette

--- src/paletted-canvas.js
+++ src/paletted-canvas.js
@@ -30,32 +30,39 @@ class IndexedImageData {
         this.#width = width;
         this.#height = height;
         this.#data = data;
-        this.palette = (palette || [[0, 0, 0]]);
+        this.palette = (palette || [[0, 0, 0, 0]]);
     }
 
-    // To get the palette index at x as a triplet of 8-bit RGB values, do "palette[x]".
-    // To modify individual indices of the returned palette, do "palette[x] = [x, x, x]".
-    // To replace the entire palette, do "palette = [[x, x, x], [x, x, x], ...]".
+    // To get the palette index at x as a quadruplet of 8-bit RGBA values, do "palette[x]".
+    // To modify individual indices of the returned palette, do "palette[x] = [R, G, B, A]".
+    // To replace the entire palette, do "palette = [[R, G, B, A], [R, G, B, A], ...]".
+    // When setting palette data, the alpha (A) component is optional - if not defined, a
+    // default of 255 will be used.
     get palette() {
         return this.#palette;
     }
 
     // Replaces the current palette with a new palette. The new palette should be an array
-    // containing 8-bit (0-255) RGB triplet arrays; e.g. [[255, 0, 0], [0, 255, 0]] for a
-    // palette of red and green.
+    // containing 8-bit (0-255) RGBA quadruplet arrays; e.g. [[255, 0, 0, 255], [0, 255, 0, 255]]
+    // for a palette of red and green (the alpha component is optional and will default to
+    // 255 if not given).
     set palette(newPalette) {
-        if (
-            !Array.isArray(newPalette) ||
-            !newPalette.every(el=>el.length === 3)
-        ){
-            throw new Error("The palette must be an array of three-element arrays.");
+        if (!Array.isArray(newPalette)) {
+            throw new Error("The palette must be an array.");
         }
+
+        newPalette.forEach(color=>{
+            color.length = 4;
+            if (typeof color[3] === "undefined") {
+                color[3] = 255;
+            }
+        });
 
         newPalette = newPalette.map(color=>Uint8ClampedArray.from(color));
 
         const palette = {
             byte: newPalette,
-            dword: new Uint32Array(newPalette.map(color=>((255 << 24) | (color[2] << 16) | (color[1] << 8) | color[0]))),
+            dword: new Uint32Array(newPalette.map(color=>((color[3] << 24) | (color[2] << 16) | (color[1] << 8) | color[0]))),
         };
 
         // We use a proxy to allow "this.#palette[x] = ..." to modify individual indices even
